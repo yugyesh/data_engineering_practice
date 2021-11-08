93546ec336754f261c868047d6b477d80e945b9a
yugyesh <uges.kk@gmail.com>
2021-11-08 09:29:32 +0900

    feat: Add spark practice

---
+++ spark_practice/Data Wrangling/maps.py
@@ -0,0 +1,27 @@
+import pyspark
+
+sc = pyspark.SparkContext(appName="maps_and_lazy_evaluation")
+
+log_of_songs = [
+    "Despacito",
+    "Nice for what",
+    "No tears left to cry",
+    "Despacito",
+    "Havana",
+    "In my feelings",
+    "Nice for what",
+    "despacito",
+    "All the stars",
+]
+
+# parallelize the log_of_songs to use with Spark
+distributed_song_log = sc.parallelize(log_of_songs)
+
+
+def convert_song_to_lowercase(song):
+    return song.lower()
+
+
+distributed_song_log.map(convert_song_to_lowercase)
+
+distributed_song_log.map(convert_song_to_lowercase).collect()
